368b4b22ba00b02ec0f88f56456b833b58215636
stefanionescu <stefanionescu@protonmail.com>
2021-01-31 01:09:37 +0000

    First full implementation

--- src/TreasuryCoreParamAdjuster.sol
+++ src/TreasuryCoreParamAdjuster.sol
@@ -20,12 +20,14 @@ contract TreasuryCoreParamAdjuster {
     }
 
     // --- Variables ---
-    uint256                  public updateDelay;                  // [seconds]
-    uint256                  public lastUpdateTime;               // [unit timestamp]
-    uint256                  public treasuryCapacityTargetValue;  // [ray]
-    uint256                  public minTreasuryCapacity;          // [rad]
-    uint256                  public minimumFundsTargetValue;      // [ray]
-    uint256                  public minMinimumFunds;              // [rad]
+    uint256                  public updateDelay;                       // [seconds]
+    uint256                  public lastUpdateTime;                    // [unit timestamp]
+    uint256                  public treasuryCapacityTargetValue;       // [ray]
+    uint256                  public minTreasuryCapacity;               // [rad]
+    uint256                  public minimumFundsTargetValue;           // [ray]
+    uint256                  public minMinimumFunds;                   // [rad]
+    uint256                  public pullFundsMinThresholdTargetValue;  // [ray]
+    uint256                  public minPullFundsThreshold;             // [rad]
 
     OracleRelayerLike        public oracleRelayer;
     StabilityFeeTreasuryLike public treasury;
@@ -35,6 +37,7 @@ contract TreasuryCoreParamAdjuster {
     event RemoveAuthorization(address account);
     event ModifyParameters(bytes32 parameter, uint256 val);
     event ModifyParameters(bytes32 parameter, address addr);
+    event UpdateTreasuryParameters(uint256 newMinPullFundsThreshold, uint256 newMinimumFunds, uint256 newTreasuryCapacity);
 
     constructor(
       address treasury_,
@@ -44,7 +47,9 @@ contract TreasuryCoreParamAdjuster {
       uint256 treasuryCapacityTargetValue_,
       uint256 minTreasuryCapacity_,
       uint256 minimumFundsTargetValue_,
-      uint256 minMinimumFunds_
+      uint256 minMinimumFunds_,
+      uint256 pullFundsMinThresholdTargetValue_,
+      uint256 minPullFundsThreshold_
     ) public {
         require(treasury_ != address(0), "TreasuryCoreParamAdjuster/null-treasury");
         require(oracleRelayer_ != address(0), "TreasuryCoreParamAdjuster/null-oracle-relayer");
@@ -54,18 +59,22 @@ contract TreasuryCoreParamAdjuster {
         require(minimumFundsTargetValue_ > 0, "TreasuryCoreParamAdjuster/null-min-funds-value");
         require(minTreasuryCapacity_ > 0, "TreasuryCoreParamAdjuster/invalid-min-capacity");
         require(minMinimumFunds_ > 0, "TreasuryCoreParamAdjuster/invalid-min-minimum-funds");
+        require(pullFundsMinThresholdTargetValue_ > 0, "TreasuryCoreParamAdjuster/null-pull-funds-threshold-value");
+        require(minPullFundsThreshold_ > 0, "TreasuryCoreParamAdjuster/null-min-pull-funds-threshold");
 
-        authorizedAccounts[msg.sender] = 1;
+        authorizedAccounts[msg.sender]   = 1;
 
-        treasury                    = StabilityFeeTreasuryLike(treasury_);
-        oracleRelayer               = OracleRelayerLike(oracleRelayer_);
+        treasury                         = StabilityFeeTreasuryLike(treasury_);
+        oracleRelayer                    = OracleRelayerLike(oracleRelayer_);
 
-        updateDelay                 = updateDelay_;
-        lastUpdateTime              = lastUpdateTime_;
-        treasuryCapacityTargetValue = treasuryCapacityTargetValue_;
-        minTreasuryCapacity         = minTreasuryCapacity_;
-        minimumFundsTargetValue     = minimumFundsTargetValue_;
-        minMinimumFunds             = minMinimumFunds_;
+        updateDelay                      = updateDelay_;
+        lastUpdateTime                   = lastUpdateTime_;
+        treasuryCapacityTargetValue      = treasuryCapacityTargetValue_;
+        minTreasuryCapacity              = minTreasuryCapacity_;
+        minimumFundsTargetValue          = minimumFundsTargetValue_;
+        minMinimumFunds                  = minMinimumFunds_;
+        pullFundsMinThresholdTargetValue = pullFundsMinThresholdTargetValue_;
+        minPullFundsThreshold            = minPullFundsThreshold_;
 
         emit AddAuthorization(msg.sender);
         emit ModifyParameters("treasury", treasury_);
@@ -76,6 +85,8 @@ contract TreasuryCoreParamAdjuster {
         emit ModifyParameters("minMinimumFunds", minMinimumFunds);
         emit ModifyParameters("treasuryCapacityTargetValue", treasuryCapacityTargetValue);
         emit ModifyParameters("minimumFundsTargetValue", minimumFundsTargetValue);
+        emit ModifyParameters("pullFundsMinThresholdTargetValue", pullFundsMinThresholdTargetValue);
+        emit ModifyParameters("minPullFundsThreshold", minPullFundsThreshold);
     }
 
     // --- Math ---
@@ -89,6 +100,11 @@ contract TreasuryCoreParamAdjuster {
     }
     function maximum(uint256 x, uint256 y) internal pure returns (uint256 z) {
         z = (x >= y) ? x : y;
+    }
+
+    // --- Boolean Logic ---
+    function both(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := and(x, y)}
     }
 
     // --- Administration ---
@@ -110,6 +126,12 @@ contract TreasuryCoreParamAdjuster {
         else if (parameter == "minMinimumFunds") {
             minMinimumFunds = val;
         }
+        else if (parameter == "pullFundsMinThresholdTargetValue") {
+            pullFundsMinThresholdTargetValue = val;
+        }
+        else if (parameter == "minPullFundsThreshold") {
+            minPullFundsThreshold = val;
+        }
         else revert("TreasuryCoreParamAdjuster/modify-unrecognized-param");
         emit ModifyParameters(parameter, val);
     }
@@ -129,15 +151,22 @@ contract TreasuryCoreParamAdjuster {
         lastUpdateTime = now;
 
         // Calculate new params
-        uint256 latestRedemptionPrice = oracleRelayer.redemptionPrice();
+        uint256 latestRedemptionPrice    = oracleRelayer.redemptionPrice();
 
-        uint256 newTreasuryCapacity   = multiply(multiply(treasuryCapacityTargetValue, WAD) / latestRedemptionPrice, RAY);
-        newTreasuryCapacity           = maximum(newTreasuryCapacity, minTreasuryCapacity);
+        uint256 newTreasuryCapacity      = multiply(multiply(treasuryCapacityTargetValue, WAD) / latestRedemptionPrice, RAY);
+        newTreasuryCapacity              = maximum(newTreasuryCapacity, minTreasuryCapacity);
 
-        uint256 newMinimumFunds       = multiply(multiply(minimumFundsTargetValue, WAD) / latestRedemptionPrice, RAY);
-        newMinimumFunds               = maximum(newMinimumFunds, minMinimumFunds);
+        uint256 newMinimumFunds          = multiply(multiply(minimumFundsTargetValue, WAD) / latestRedemptionPrice, RAY);
+        newMinimumFunds                  = maximum(newMinimumFunds, minMinimumFunds);
+
+        uint256 newMinPullFundsThreshold = multiply(multiply(pullFundsMinThresholdTargetValue, WAD) / latestRedemptionPrice, RAY);
+        newMinPullFundsThreshold         = maximum(newMinPullFundsThreshold, minPullFundsThreshold);
 
         // Set parameters
-        
+        treasury.modifyParameters("pullFundsMinThreshold", newMinPullFundsThreshold);
+        treasury.modifyParameters("minimumFundsRequired", newMinimumFunds);
+        treasury.modifyParameters("treasuryCapacity", newTreasuryCapacity);
+
+        emit UpdateTreasuryParameters(newMinPullFundsThreshold, newMinimumFunds, newTreasuryCapacity);
     }
 }
